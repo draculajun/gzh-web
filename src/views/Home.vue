514ce691e7ad77914e60143d7a121659a344e596
wenjun.wang@athub.com <wangwj123>
2021-07-17 20:08:34 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,49 @@
 <template>
-    <div>
-        <!--<van-nav-bar title="标题" left-text="返回" left-arrow @click-left="navBarLeftHandler"></van-nav-bar>-->
-        <router-view></router-view>
+  <div>
+    <!--<van-nav-bar title="标题" left-text="返回" left-arrow @click-left="navBarLeftHandler"></van-nav-bar>-->
+    <router-view></router-view>
 
-        <van-tabbar v-model="active" route>
-            <van-tabbar-item to="/index" icon="home-o">Tab1</van-tabbar-item>
-            <van-tabbar-item to="/Tab2" icon="search">Tab2</van-tabbar-item>
-            <van-tabbar-item icon="friends-o">Tab3</van-tabbar-item>
-        </van-tabbar>
-    </div>
+    <van-tabbar v-model="active" route>
+      <van-tabbar-item to="/index" icon="home-o">Tab1</van-tabbar-item>
+      <van-tabbar-item to="/Tab2" icon="search">Tab2</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">Tab3</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
 
-    import {Toast} from "vant";
+import {Toast} from "vant";
 
-    export default {
-        name: "Home",
+export default {
+  name: "Home",
 
-        data() {
-            return {
-                active: 0,
-            }
-        },
-
-        methods: {
-            sayHello() {
-                Toast('hello');
-            },
-
-            btnRedirectHandler() {
-                this.$router.push({
-                    'name': 'World',
-                })
-            },
-
-            navBarLeftHandler() {
-                this.$router.back(-1);
-            },
-        },
-
-        mounted() {
-          this.active = 0;
-        }
+  data() {
+    return {
+      active: 0,
     }
+  },
+
+  methods: {
+    sayHello() {
+      Toast('hello');
+    },
+
+    btnRedirectHandler() {
+      this.$router.push({
+        'name': 'World',
+      })
+    },
+
+    navBarLeftHandler() {
+      this.$router.back(-1);
+    },
+  },
+
+  mounted() {
+    this.active = 0;
+  }
+}
 </script>
 
 <style scoped>
--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -1,28 +1,27 @@
 <template>
-    <van-cell-group>
-        <van-cell>
-            <h1>TAB2</h1>
-        </van-cell>
-        <van-cell center title="获取ACCESSTOKEN">
-            <van-button @click="btnGetAccessTokenHandler">getAccessToken</van-button>
-        </van-cell>
-    </van-cell-group>
+  <van-cell-group>
+    <van-cell>
+      <h1>TAB2</h1>
+    </van-cell>
+    <van-cell center title="获取ACCESSTOKEN">
+      <van-button @click="btnGetAccessTokenHandler">getAccessToken</van-button>
+    </van-cell>
+  </van-cell-group>
 </template>
 
 <script>
-    import {Toast} from "vant";
-    // import
+import {Toast} from "vant";
+// import
 
-    export default {
-        name: "Tab2",
+export default {
+  name: "Tab2",
 
-        methods: {
-            btnGetAccessTokenHandler() {
-
-                Toast('aaa');
-            },
-        }
-    }
+  methods: {
+    btnGetAccessTokenHandler() {
+      Toast('aaa');
+    },
+  }
+}
 </script>
 
 <style scoped>
