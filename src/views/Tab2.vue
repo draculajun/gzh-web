b73ba14c095dd58225cf36eef94eec33d0fcf556
wenjun.wang@athub.com <wangwj123>
2021-07-20 15:39:41 +0800

    环境变量

--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -1,47 +1,47 @@
 <template>
-  <van-cell-group>
-    <van-cell>
-      <h1>TAB2</h1>
-    </van-cell>
-    <van-cell center title="获取ACCESSTOKEN">
-      <van-button type="primary" @click="btnGetAccessTokenHandler">确定</van-button>
-    </van-cell>
+    <van-cell-group>
+        <van-cell>
+            <h1>TAB2</h1>
+        </van-cell>
+        <van-cell center title="获取ACCESSTOKEN">
+            <van-button type="primary" @click="btnGetAccessTokenHandler">确定</van-button>
+        </van-cell>
 
-    <van-cell center title="新增永久图片素材">
-      <van-uploader :after-read="btnAddOtherMaterialHandler"/>
-      <!--      <van-button type="primary" @click="btnAddOtherMaterialHandler">确定</van-button>-->
-    </van-cell>
-  </van-cell-group>
+        <van-cell center title="新增永久图片素材">
+            <van-uploader :after-read="btnAddOtherMaterialHandler"/>
+            <!--      <van-button type="primary" @click="btnAddOtherMaterialHandler">确定</van-button>-->
+        </van-cell>
+    </van-cell-group>
 </template>
 
 <script>
 
-import {Toast} from "vant";
+    import {Toast} from "vant";
 
-export default {
-  name: "Tab2",
+    export default {
+        name: "Tab2",
 
-  methods: {
-    btnGetAccessTokenHandler() {
-      this.$wxAxios.get('/accessToken').then(res => {
-        console.log('accessToken:' + res.data);
-        Toast('accessToken:' + res.data);
-      }).catch(err => {
-        console.log(err);
-      });
-    },
+        methods: {
+            btnGetAccessTokenHandler() {
+                this.$wxAxios.get('/accessToken').then(res => {
+                    console.log('accessToken:' + res.data);
+                    Toast('accessToken:' + res.data);
+                }).catch(err => {
+                    console.log(err);
+                });
+            },
 
-    btnAddOtherMaterialHandler(obj) {
-      console.log(obj);
-      let formData = new window.FormData();
-      formData.append("file", obj.file);
-      formData.append("type", 'image');
-      this.$wxAxios.post('/material/others', formData, {'Content-Type': 'multipart/form-data'}).then(res => {
-        console.log(res);
-      })
-    },
-  }
-}
+            btnAddOtherMaterialHandler(obj) {
+                console.log(obj);
+                let formData = new window.FormData();
+                formData.append("file", obj.file);
+                formData.append("type", 'image');
+                this.$wxAxios.post('/material/others', formData, {'Content-Type': 'multipart/form-data'}).then(res => {
+                    console.log(res);
+                })
+            },
+        }
+    }
 </script>
 
 <style scoped>
--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,13 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {   //代理api，解决跨域问题
-                target: 'http://www.dracula.host',
+                target: process.env.VUE_APP_WX_URL,
                 ws: true,
                 changOrigin: true,
                 pathRewrite: {
                     '^/api': ''
                 }
             }
-        }
+        },
     }
 }
