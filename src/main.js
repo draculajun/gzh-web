320783b5d96ea8e46d7d8d281386309684e0cca8
draculajun <34988153@qq.com>
2021-07-29 22:36:49 +0800

    elementui

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import baseAxios from "@/utils/request";
 
 import {Button, Cell, CellGroup, Grid, GridItem, NavBar, Tabbar, TabbarItem, Uploader} from 'vant'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.config.productionTip = false
 
 // 不需要每次都 import一下 axios了，直接使用 $wxAxios 即可
@@ -20,6 +23,8 @@ Vue.use(TabbarItem);
 Vue.use(Grid);
 Vue.use(GridItem);
 Vue.use(Uploader);
+
+Vue.use(ElementUI);
 
 Vue.use(router);
 
